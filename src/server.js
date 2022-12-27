c6f69817bb6ef56cb40f2ace5b8548c0ddd555f6
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-12-27 09:52:15 +0700

    Configure to connect to MongoDB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const Hapi = require('@hapi/hapi');
+const mongoDB = require('hapi-mongodb');
+const dotenv = require('dotenv');
 
 const init = async () => {
+  dotenv.config();
+
   const server = Hapi.server({
     port: process.env.PORT || 80,
     host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
@@ -8,6 +12,18 @@ const init = async () => {
       cors: {
         origin: ['*'],
       },
+    },
+  });
+
+  // Configure MongoDB
+  await server.register({
+    plugin: mongoDB,
+    options: {
+      url: process.env.MONGODB_URI,
+      settings: {
+        useUnifiedTopology: true,
+      },
+      decorate: true,
     },
   });
 
